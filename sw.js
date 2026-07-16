55ea3a5ad38e20709c4020d2ad227dce5d5fe8fc
dealingcards <donsibley01@gmail.com>
2026-07-16 11:46:04 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='rs-v2';
-const ASSETS=['./index.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>{const f=fetch(e.request).then(r=>{if(r&&r.status===200)caches.open(CACHE).then(cache=>cache.put(e.request,r.clone()));return r}).catch(()=>c);return c||f}))});
